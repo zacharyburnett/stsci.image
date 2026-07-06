d3b7affcf51fea3ae95c45e185fc91e6f6f67387
zacharyburnett <zburnett@stsci.edu>
2026-07-06 13:23:16 -0400

    [SCSB-237] build Stable ABI3 wheels with `Py_LIMITED_API`

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from numpy import get_include as np_include
 from setuptools import Extension, setup
 
+MACROS = [
+    ("Py_LIMITED_API", 0x03090000),  # PY_VERSION_HEX for 3.9
+]
+
 ext_modules = [
     Extension(
         "stsci.image._combine",
         ["src/_combinemodule.c"],
         include_dirs=[np_include()],
+        define_macros=MACROS,
     ),
 ]
 
 setup(
     ext_modules=ext_modules,
+    options={'bdist_wheel': {'py_limited_api': 'cp39'}},
 )
